071ad252e73ab86ec0e40f24edaa442ff165bcb1
유충열 <kchot10@gmail.com>
2023-05-02 01:23:49 +0900

    Add second kakao maps

--- app/src/main/java/com/cookandroid/travelerapplication/kotlin/KotlinActivity.kt
+++ app/src/main/java/com/cookandroid/travelerapplication/kotlin/KotlinActivity.kt
@@ -1,12 +1,153 @@
 package com.cookandroid.travelerapplication.kotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.cookandroid.travelerapplication.R
+import net.daum.mf.map.api.MapPOIItem
+import net.daum.mf.map.api.MapPoint
+import net.daum.mf.map.api.MapView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class KotlinActivity : AppCompatActivity() {
+    companion object {
+        const val BASE_URL = "https://dapi.kakao.com/"
+        const val API_KEY = "KakaoAK b066261c4e1af27a764a28804ace4ab2"  // REST API 키
+    }
+
+    private val listItems = arrayListOf<ListLayout>()   // 리사이클러 뷰 아이템
+    private val listAdapter = ListAdapter(listItems)    // 리사이클러 뷰 어댑터
+    private var pageNumber = 1      // 검색 페이지 번호
+    private var keyword = ""        // 검색 키워드
+
+    private lateinit var mapView: MapView
+    private lateinit var rvList: RecyclerView
+    private lateinit var etSearchField: EditText
+    private lateinit var tvPageNumber: TextView
+    private lateinit var btnSearch: Button
+    private lateinit var btnPrevPage: Button
+    private lateinit var btnNextPage: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin)
+
+        mapView = findViewById(R.id.mapView)
+        rvList = findViewById(R.id.rv_list)
+        btnSearch = findViewById(R.id.btn_search)
+        btnPrevPage = findViewById(R.id.btn_prevPage)
+        btnNextPage = findViewById(R.id.btn_nextPage)
+        etSearchField = findViewById(R.id.et_search_field)
+        tvPageNumber = findViewById(R.id.tv_pageNumber)
+
+
+        // 리사이클러 뷰
+        rvList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        rvList.adapter = listAdapter
+        // 리스트 아이템 클릭 시 해당 위치로 이동
+        listAdapter.setItemClickListener(object: ListAdapter.OnItemClickListener {
+            override fun onClick(v: View, position: Int) {
+                val mapPoint = MapPoint.mapPointWithGeoCoord(listItems[position].y, listItems[position].x)
+                Log.d("listItems", "listItems[position].category_group_code: "+listItems[position].category_group_code)
+                Log.d("listItems", "listItems[position].category_group_name: "+listItems[position].category_group_name)
+                mapView.setMapCenterPointAndZoomLevel(mapPoint, 1, true)
+            }
+        })
+
+        // 검색 버튼
+        btnSearch.setOnClickListener {
+            keyword = etSearchField.text.toString()
+            pageNumber = 1
+            searchKeyword(keyword, pageNumber)
+        }
+
+        // 이전 페이지 버튼
+        btnPrevPage.setOnClickListener {
+            pageNumber--
+            tvPageNumber.text = pageNumber.toString()
+            searchKeyword(keyword, pageNumber)
+        }
+
+        // 다음 페이지 버튼
+        btnNextPage.setOnClickListener {
+            pageNumber++
+            tvPageNumber.text = pageNumber.toString()
+            searchKeyword(keyword, pageNumber)
+        }
     }
+
+    // 키워드 검색 함수
+    private fun searchKeyword(keyword: String, page: Int) {
+        val retrofit = Retrofit.Builder()          // Retrofit 구성
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val api = retrofit.create(KakaoAPI::class.java)            // 통신 인터페이스를 객체로 생성
+        val call = api.getSearchKeyword(API_KEY, keyword, page)    // 검색 조건 입력
+
+        // API 서버에 요청
+        call.enqueue(object: Callback<ResultSearchKeyword> {
+            override fun onResponse(call: Call<ResultSearchKeyword>, response: Response<ResultSearchKeyword>) {
+                // 통신 성공
+                addItemsAndMarkers(response.body())
+            }
+
+            override fun onFailure(call: Call<ResultSearchKeyword>, t: Throwable) {
+                // 통신 실패
+                Log.w("LocalSearch", "통신 실패: ${t.message}")
+            }
+        })
+    }
+
+    // 검색 결과 처리 함수
+    private fun addItemsAndMarkers(searchResult: ResultSearchKeyword?) {
+        if (!searchResult?.documents.isNullOrEmpty()) {
+            // 검색 결과 있음
+            listItems.clear()                   // 리스트 초기화
+            mapView.removeAllPOIItems() // 지도의 마커 모두 제거
+            for (document in searchResult!!.documents) {
+                // 결과를 리사이클러 뷰에 추가
+                val item = ListLayout(document.place_name,
+                    document.road_address_name,
+                    document.address_name,
+                    document.x.toDouble(),
+                    document.y.toDouble(),
+                    document.category_group_code,
+                    document.category_group_name)
+                listItems.add(item)
+
+                // 지도에 마커 추가
+                val point = MapPOIItem()
+                point.apply {
+                    itemName = document.place_name
+                    mapPoint = MapPoint.mapPointWithGeoCoord(document.y.toDouble(),
+                        document.x.toDouble())
+                    markerType = MapPOIItem.MarkerType.BluePin
+                    selectedMarkerType = MapPOIItem.MarkerType.RedPin
+                }
+                mapView.addPOIItem(point)
+            }
+            listAdapter.notifyDataSetChanged()
+
+            btnNextPage.isEnabled = !searchResult.meta.is_end // 페이지가 더 있을 경우 다음 버튼 활성화
+            btnPrevPage.isEnabled = pageNumber != 1             // 1페이지가 아닐 경우 이전 버튼 활성화
+
+        } else {
+            // 검색 결과 없음
+            Toast.makeText(this, "검색 결과가 없습니다", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
---
+++ app/src/main/java/com/cookandroid/travelerapplication/kotlin/ListLayout.kt
@@ -0,0 +1,9 @@
+package com.cookandroid.travelerapplication.kotlin
+
+class ListLayout(val name: String,      // 장소명
+                 val road: String,      // 도로명 주소
+                 val address: String,   // 지번 주소
+                 val x: Double,         // 경도(Longitude)
+                 val y: Double,   // 지번 주소
+                 val category_group_code: String,         // 경도(Longitude)
+                 val category_group_name: String)         // 위도(Latitude)
