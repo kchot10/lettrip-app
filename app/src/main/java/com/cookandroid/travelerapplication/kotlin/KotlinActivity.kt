3becc2e25785fa73f8d78552c45cf087b976c2e9
유충열 <kchot10@gmail.com>
2023-05-02 00:49:33 +0900

    Add first kakao maps

---
+++ app/src/main/java/com/cookandroid/travelerapplication/kotlin/KotlinActivity.kt
@@ -0,0 +1,12 @@
+package com.cookandroid.travelerapplication.kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cookandroid.travelerapplication.R
+
+class KotlinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin)
+    }
+}
