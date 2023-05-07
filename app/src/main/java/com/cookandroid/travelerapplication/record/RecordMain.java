d91563a8c3f57f6756a97805a34f221dafd6315b
서정후 <jeonghu1010@naver.com>
2023-05-07 16:54:30 +0900

    후기 기록_도시 선택 드롭다운 추가

--- app/src/main/java/com/cookandroid/travelerapplication/record/RecordMain.java
+++ app/src/main/java/com/cookandroid/travelerapplication/record/RecordMain.java
@@ -1,14 +1,20 @@
 package com.cookandroid.travelerapplication.record;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.PopupMenu;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
@@ -17,7 +23,10 @@ import com.cookandroid.travelerapplication.R;
 
 public class RecordMain extends AppCompatActivity {
     private EditText edittext_title;
-    Spinner spinner;
+    Spinner spinner; //지역 구분1
+    Spinner spinner2; //지역 구분2
+    ArrayAdapter<CharSequence> adapter;
+    ArrayAdapter<CharSequence> adapter2;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,58 +35,48 @@ public class RecordMain extends AppCompatActivity {
         edittext_title = findViewById(R.id.edittext_title);
         String title = String.valueOf(edittext_title.getText()); //제목 받아오기 - db 저장 필요
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.my_array_state, R.layout.spinner_layout);
-        adapter.setDropDownViewResource(com.airbnb.lottie.R.layout.support_simple_spinner_dropdown_item);
+        adapter = ArrayAdapter.createFromResource(this, R.array.my_array_state, R.layout.spinner_layout);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner = findViewById(R.id.cityDropdown);
         spinner.setAdapter(adapter);
 
-    }
+        adapter2 = ArrayAdapter.createFromResource(this, R.array.city_array_default, R.layout.spinner_layout);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2 = findViewById(R.id.cityDropdown_detail);
+        spinner2.setAdapter(adapter2);
 
-    public void spinnerListener(){
+
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
+                String selectedCity;
                 switch(position){
+                    case 0:
+                        setCitySpinnerAdapterItem(R.array.city_array_default);
+                        selectedCity = spinner2.getSelectedItem().toString(); //도시명 받아오기 - db 저장 필요
+                        break;
                     case 1:
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        break;
-                    case 4:
-                        break;
-                    case 5:
-                        break;
-                    case 6:
-                        break;
-                    case 7:
-                        break;
-                    case 8:
-                        break;
-                    case 9:
-                        break;
-                    case 10:
-                        break;
-                    case 11:
-                        break;
-                    case 12:
-                        break;
-                    case 13:
-                        break;
-                    case 14:
-                        break;
-                    case 15:
-                        break;
-                    case 16:
-                        break;
-                    case 17:
+                        setCitySpinnerAdapterItem(R.array.서울특별시);
                         break;
                 }
-            }
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
 
             }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Toast toast = Toast.makeText(getApplicationContext(), "도시를 선택하세요.", Toast.LENGTH_LONG);
+            }
         });
+
+
     }
+
+    private void setCitySpinnerAdapterItem(int array_resource) {
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, (String[])getResources().getStringArray(array_resource));
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter);
+    }
+
+
 }
