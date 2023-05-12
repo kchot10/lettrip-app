b60a1dd6c90fc1285a1b72290cb941e1be715aa0
유충열 <kchot10@gmail.com>
2023-05-13 04:38:04 +0900

    Update CourseActivity.java (Detail)

--- app/src/main/java/com/cookandroid/travelerapplication/travel/CourseActivity.java
+++ app/src/main/java/com/cookandroid/travelerapplication/travel/CourseActivity.java
@@ -20,7 +20,10 @@ import com.cookandroid.travelerapplication.kotlin.KotlinActivity;
 import com.cookandroid.travelerapplication.task.InsertData_Course;
 import com.cookandroid.travelerapplication.task.InsertData_Place;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
@@ -30,6 +33,8 @@ public class CourseActivity extends AppCompatActivity {
     String IP_ADDRESS;
     FileHelper fileHelper;
     String city, province, depart_date;
+
+    Button button_place_search;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,26 +44,37 @@ public class CourseActivity extends AppCompatActivity {
         province = getIntent().getStringExtra("province");
         city = getIntent().getStringExtra("city");
         depart_date = getIntent().getStringExtra("depart_date");
+        button_place_search = findViewById(R.id.button_place_search);
+        EditText editText_day_count = findViewById(R.id.editText_day_count);
+        EditText editText_arrived_time_hour = findViewById(R.id.editText_arrived_time_hour);
+        EditText editText_arrived_time_min = findViewById(R.id.editText_arrived_time_min);
+        EditText editText_cost = findViewById(R.id.editText_cost);
 
 
         findViewById(R.id.button_add_cource).setOnClickListener(v -> {
-            EditText editText_arrived_time_hour = findViewById(R.id.editText_arrived_time_hour);
-            EditText editText_arrived_time_min = findViewById(R.id.editText_arrived_time_min);
-            String hour = editText_arrived_time_hour.getText().toString().trim();
-            String min = editText_arrived_time_min.getText().toString().trim();
-            EditText editText_cost = findViewById(R.id.editText_cost);
-            EditText editText_day_count = findViewById(R.id.editText_day_count);
+            if (button_place_search.getText().toString().trim().equals("장소 검색")){
+                Toast.makeText(this, "장소를 입력하세요", Toast.LENGTH_SHORT).show();
+            } else if (editText_day_count.getText().toString().trim().equals("")) {
+                Toast.makeText(this, "일차를 입력하세요", Toast.LENGTH_SHORT).show();
+            } else if (editText_arrived_time_min.getText().toString().trim().equals("")||editText_arrived_time_hour.getText().toString().trim().equals("")) {
+                Toast.makeText(this, "도착 시간을 입력하세요", Toast.LENGTH_SHORT).show();
+            } else if (editText_cost.getText().toString().trim().equals("")) {
+                Toast.makeText(this, "비용을 입력하세요", Toast.LENGTH_SHORT).show();
+            } else {
+                String hour = editText_arrived_time_hour.getText().toString().trim();
+                String min = editText_arrived_time_min.getText().toString().trim();
+                String cost = editText_cost.getText().toString().trim();
+                String day_count = editText_day_count.getText().toString().trim();
+                depart_date = addDays(depart_date, day_count);
+                String arrived_time = getCurrentTime_custom(hour, min);
+                String place_id = fileHelper.readFromFile("place_id");
+                String review_review_id = "1";
+                String travel_id = fileHelper.readFromFile("travel_id");
 
-            String arrived_time = getCurrentTime_custom(hour, min);
-            String cost = editText_cost.getText().toString().trim();
-            String day_count = editText_day_count.getText().toString().trim();
-            String place_id = fileHelper.readFromFile("place_id");
-            String review_review_id = "1";
-            String travel_id = fileHelper.readFromFile("travel_id");
-
-            InsertData_Course insertData_course = new InsertData_Course();
-            insertData_course.execute("http://"+IP_ADDRESS+"/0503/InsertData_Course.php",arrived_time,cost,day_count, place_id,review_review_id, travel_id);
-            finish();
+                InsertData_Course insertData_course = new InsertData_Course();
+                insertData_course.execute("http://"+IP_ADDRESS+"/0503/InsertData_Course.php",arrived_time,cost,day_count, place_id,review_review_id, travel_id);
+                finish();
+            }
         });
 
         findViewById(R.id.button_place_search).setOnClickListener(v -> {
@@ -78,11 +94,23 @@ public class CourseActivity extends AppCompatActivity {
         return currentTime;
     }
 
+    private String addDays(String date, String days) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+        Calendar cal = Calendar.getInstance();
+        try {
+            cal.setTime(sdf.parse(date));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        cal.add(Calendar.DATE, Integer.parseInt(days));
+        return sdf.format(cal.getTime());
+    }
+
+
     private final ActivityResultLauncher<Intent> getKotlinActivityResult = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             result -> {
                 if (result.getResultCode() == RESULT_OK){
-                    Button button_place_search = findViewById(R.id.button_place_search);
                     button_place_search.setText(result.getData().getStringExtra("name"));
                     String place_name = result.getData().getStringExtra("name");
                     String road = result.getData().getStringExtra("road");
