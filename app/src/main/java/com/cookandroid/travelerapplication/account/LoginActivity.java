f882e0aa9faf271ee8b5c36e81f81fe475e71395
유충열 <kchot10@gmail.com>
2023-05-19 12:56:15 +0900

    Update RecordMain.java (이미지 파일 불러오기 추가 (찐))

--- app/src/main/java/com/cookandroid/travelerapplication/account/LoginActivity.java
+++ app/src/main/java/com/cookandroid/travelerapplication/account/LoginActivity.java
@@ -59,7 +59,7 @@ import org.mindrot.jbcrypt.BCrypt;
 public class LoginActivity extends AppCompatActivity{
 
 
-    private static String ec2_ADDRESS = "43.201.111.57";
+    private static String ec2_ADDRESS = "13.125.172.111";
     private static String IP_ADDRESS;
     public static Context mContext;
     FileHelper fileHelper;
