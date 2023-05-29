fcc977db027ed93872be4796644fdccad70af4a7
서정후 <jeonghu1010@naver.com>
2023-05-30 02:09:57 +0900

    added trip mission design

---
+++ app/src/main/java/com/cookandroid/travelerapplication/mission/MissionTripActivity.java
@@ -0,0 +1,16 @@
+package com.cookandroid.travelerapplication.mission;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.cookandroid.travelerapplication.R;
+
+public class MissionTripActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mission_trip);
+    }
+}
---
+++ app/src/main/java/com/cookandroid/travelerapplication/mission/missionMainActivity.java
@@ -0,0 +1,27 @@
+package com.cookandroid.travelerapplication.mission;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.cookandroid.travelerapplication.R;
+
+public class missionMainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mission_main);
+
+        LinearLayout missionLayout = findViewById(R.id.missionTrip);
+        missionLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MissionTripActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
