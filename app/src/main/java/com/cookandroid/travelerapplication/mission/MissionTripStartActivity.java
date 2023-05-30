08e49b81203459db8e3f20b29ca87c13606652ef
서정후 <jeonghu1010@naver.com>
2023-05-30 21:02:43 +0900

    Mission timer 수정

--- app/src/main/java/com/cookandroid/travelerapplication/mission/MissionTripStartActivity.java
+++ app/src/main/java/com/cookandroid/travelerapplication/mission/MissionTripStartActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
@@ -18,9 +19,23 @@ import com.cookandroid.travelerapplication.R;
 import com.cookandroid.travelerapplication.helper.FileHelper;
 import com.cookandroid.travelerapplication.task.InsertData_Mission;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -30,10 +45,6 @@ public class MissionTripStartActivity extends AppCompatActivity {
     int sum = 0;
     int sec;
     int min;
-    private LocationManager locationManager;
-    double current_latitude;
-    double current_longitude;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,8 +54,6 @@ public class MissionTripStartActivity extends AppCompatActivity {
         IP_ADDRESS = fileHelper.readFromFile("IP_ADDRESS");
         user_id = fileHelper.readFromFile("user_id");
         ImageButton backBtn = findViewById(R.id.leftArrowBtn);
-        FrameLayout map_view = findViewById(R.id.map_FramLayout);
-        TextView textView = findViewById(R.id.time_TextView);
 
         findViewById(R.id.missionVerificationBtn).setOnClickListener(v -> {
             sum+=1;
@@ -57,7 +66,6 @@ public class MissionTripStartActivity extends AppCompatActivity {
 
 
 
-
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -66,9 +74,82 @@ public class MissionTripStartActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        //1km 이내의 장소 리스트 불러오기
 
+        //1km 이내의 장소 리스트 불러오기
         timerRun();
+
+        String keyword="음식점";
+        double latitude = 37.5665; // 중심 좌표의 위도
+        double longitude = 126.9780; // 중심 좌표의 경도 -> 현재 좌표로 바꾸기
+        int radius = 1000; //1000m 내의 위치 검색
+        String apiUrl = "https://dapi.kakao.com/v2/local/search/keyword.json?query=" + keyword + "&y=" + latitude + "&x=" + longitude + "&radius=" + radius;
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                List<String> placeList = new ArrayList<>();
+
+                try {
+                    URL url = new URL(apiUrl);
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setRequestProperty("Authorization", "KakaoAK 43a9d1617d8fb89af04db23790b3dd2");
+
+
+                    int responseCode = connection.getResponseCode();
+                    if (responseCode == HttpURLConnection.HTTP_OK) {
+                        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                        String inputLine;
+                        StringBuilder response = new StringBuilder();
+                        while ((inputLine = in.readLine()) != null) {
+                            response.append(inputLine);
+                        }
+                        in.close();
+
+                        // JSON 응답을 처리하는 로직 구현
+                        JSONObject jsonResponse = new JSONObject(response.toString());
+                        // documents 배열에서 장소 정보를 가져옵니다.
+                        JSONArray documents = jsonResponse.getJSONArray("documents");
+
+                        for (int i = 0; i < documents.length(); i++) {
+                            JSONObject placeObject = documents.getJSONObject(i);
+                            String placeName = placeObject.getString("place_name");
+                            placeList.add(placeName);
+                        }
+
+
+                    } else {
+                        Log.d("kakaoMap", "http요청 실패");
+                    }
+                } catch (ProtocolException e) {
+                    throw new RuntimeException(e);
+                } catch (MalformedURLException e) {
+                    throw new RuntimeException(e);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (!placeList.isEmpty()) {
+                            // placeList 리스트에서 랜덤으로 한 장소 선택
+                            Random random = new Random();
+                            int randomIndex = random.nextInt(placeList.size());
+                            String selectedPlace = placeList.get(randomIndex);
+
+                            // 선택된 장소 출력
+                            TextView placeName = findViewById(R.id.place_explanation);
+                            placeName.setText(selectedPlace);
+                        }
+                    }
+                });
+            }
+        }).start();
+
+
     }
 
     private void timerRun() {
@@ -77,7 +158,7 @@ public class MissionTripStartActivity extends AppCompatActivity {
         TextView min_TextView = findViewById(R.id.min);
         TextView sec_TextView = findViewById(R.id.sec);
 
-        min_TextView.setText("0");
+        min_TextView.setText("30");
         sec_TextView.setText("0");
 
         min = Integer.parseInt(min_TextView.getText().toString());
@@ -86,37 +167,49 @@ public class MissionTripStartActivity extends AppCompatActivity {
         Timer timer = new Timer();
         TimerTask timerTask = new TimerTask() {
             @Override
-            public void run() {
-                if(sec != 0){
-                    sec--;
-                } else if(min != 0){
-                    sec = 60;
-                    sec--;
-                    min--;
-                }
+            public void run(){
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(sec != 0){
+                            sec--;
+                            sec_TextView.setText(Integer.toString(sec));
+                        } else if(min != 0){
+                            sec = 60;
+                            sec--;
+                            min--;
+                            sec_TextView.setText(Integer.toString(sec));
+                            min_TextView.setText(Integer.toString(min));
+                        }
 
-                if(min <= 9){
-                    min_TextView.setText("0" + min);
-                } else{
-                    min_TextView.setText(min);
-                }
+                        if(min <= 9){
+                            min_TextView.setText("0" + Integer.toString(min));
+                        } else{
+                            min_TextView.setText(Integer.toString(min));
+                        }
 
-                if(sec <= 9){
-                    min_TextView.setText("0" + sec);
-                } else{
-                    min_TextView.setText(sec);
-                }
+                        if(sec <= 9){
+                            sec_TextView.setText("0" + Integer.toString(sec));
+                        } else{
+                            sec_TextView.setText(Integer.toString(sec));
+                        }
 
-                if(min == 0 && sec == 0){
-                    timer.cancel();
-                    showAlertDialog();
-                }
+                        if(min == 0 && sec == 0){
+                            timer.cancel();
+                            showAlertDialog();
+                        }
 
-                if(sec<=30){
-                    sec_TextView.setTextColor(Color.parseColor("#E12E23")); //빨간색으로 바꾸기
-                }
+                        if(sec<=30){
+                            sec_TextView.setTextColor(Color.parseColor("#E12E23")); //빨간색으로 바꾸기
+                            min_TextView.setTextColor(Color.parseColor("#E12E23")); //빨간색으로 바꾸기
+                        }
+                    }
+                });
             }
+
         };
+
+        timer.schedule(timerTask, 0, 1000); // 타이머 스케줄링 추가
     }
 
     private String getCurrentTime() {
