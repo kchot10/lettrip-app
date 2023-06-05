d7d693335827d400b4c275e8b7a52e288da1fa8e
서정후 <jeonghu1010@naver.com>
2023-06-06 03:04:39 +0900

    added mypage_userCheck code

--- app/src/main/java/com/cookandroid/travelerapplication/main/MainActivity.java
+++ app/src/main/java/com/cookandroid/travelerapplication/main/MainActivity.java
@@ -10,6 +10,7 @@ import com.cookandroid.travelerapplication.article.ArticleListActivity;
 import com.cookandroid.travelerapplication.account.WithdrawActivity;
 import com.cookandroid.travelerapplication.helper.FileHelper;
 import com.cookandroid.travelerapplication.mission.MissionMainActivity;
+import com.cookandroid.travelerapplication.mypage.MypageMainActivity;
 import com.cookandroid.travelerapplication.record.RecordMain;
 import com.cookandroid.travelerapplication.search.SearchActivity;
 
@@ -68,7 +69,10 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
-
+        findViewById(R.id.mypage).setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, MypageMainActivity.class);
+            startActivity(intent);
+        });
 
 
     }
