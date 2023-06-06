ea3c3f0ebf7f3b34f2cafe2605ef306753237b58
유충열 <kchot10@gmail.com>
2023-06-06 15:16:51 +0900

    ADD 게시글 정렬 기준 버튼 만들기(최신, 오래된, 조회의 순서 적용)

--- app/src/main/java/com/cookandroid/travelerapplication/article/ArticleListActivity.java
+++ app/src/main/java/com/cookandroid/travelerapplication/article/ArticleListActivity.java
@@ -1,8 +1,13 @@
 package com.cookandroid.travelerapplication.article;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -14,6 +19,8 @@ import com.cookandroid.travelerapplication.R;
 import com.cookandroid.travelerapplication.task.SelectData_Article;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class ArticleListActivity extends AppCompatActivity {
 
@@ -21,13 +28,15 @@ public class ArticleListActivity extends AppCompatActivity {
     private static String IP_ADDRESS; //본인 IP주소를 넣으세요.
     private RecyclerView recyclerView;
     private ArticleAdapter adapter;
+    private Spinner sorting_spinner;
+    private ArrayList<Article> articleArrayList;
+
+    private final int NEW = 1, OLD = 2, HIT = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_list);
-
-
         FileHelper fileHelper = new FileHelper(this);
         IP_ADDRESS = fileHelper.readFromFile("IP_ADDRESS");
 
@@ -37,11 +46,39 @@ public class ArticleListActivity extends AppCompatActivity {
                 new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(layoutManager);
 
+        sorting_spinner = findViewById(R.id.sorting_spinner);
 
-        Refresh();
+        sorting_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        Collections.sort(articleArrayList, (a1, a2) -> a2.getCreated_date().compareTo(a1.getCreated_date()));
+                        Refresh(NEW);
+                        break;
+                    case 1:
+                        Collections.sort(articleArrayList, (a1, a2) -> a1.getCreated_date().compareTo(a2.getCreated_date()));
+                        Refresh(OLD);
+                        break;
+                    case 2:
+                        Collections.sort(articleArrayList, (a1, a2) -> Integer.compare(Integer.parseInt(a2.getHit()), Integer.parseInt(a1.getHit())));
+                        Refresh(HIT);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+        Refresh(NEW);
+
 
         findViewById(R.id.button_refresh).setOnClickListener(v -> {
-            Refresh();
+            Refresh(NEW);
         });
 
         findViewById(R.id.button_add).setOnClickListener(v -> {
@@ -56,10 +93,12 @@ public class ArticleListActivity extends AppCompatActivity {
 
     }
 
-    public void Refresh() {
-        ArrayList<Article> articleArrayList = new ArrayList<>();
-        SelectData_Article task = new SelectData_Article(articleArrayList);
-        task.execute("http://" + IP_ADDRESS + "/0422/selectdata_article.php");
+    public void Refresh(int TYPE) {
+        if (TYPE == NEW) {
+            articleArrayList = new ArrayList<>();
+            SelectData_Article task = new SelectData_Article(articleArrayList);
+            task.execute("http://" + IP_ADDRESS + "/0422/selectdata_article.php");
+        }
 
         try {
             new Handler().postDelayed(() -> {
@@ -74,6 +113,6 @@ public class ArticleListActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        Refresh();
+        Refresh(NEW);
     }
 }
