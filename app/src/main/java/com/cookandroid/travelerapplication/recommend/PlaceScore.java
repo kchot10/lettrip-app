eab1b4e8b7eb3bf79a022a5ebc06474f7fee7745
유충열 <kchot10@gmail.com>
2023-08-25 07:11:35 +0900

    Add 추천 시스템 완성 (기능적으로 설명: 장소 추천 버튼을 누르면 추천 리스트를 출력하는 기능을 추가함. 단, 사용자에 따라 추천 장소가 달라짐)

---
+++ app/src/main/java/com/cookandroid/travelerapplication/recommend/PlaceScore.java
@@ -0,0 +1,22 @@
+package com.cookandroid.travelerapplication.recommend;
+
+public class PlaceScore {
+    String placeName;
+    String score;
+
+    public String getPlaceName() {
+        return placeName;
+    }
+
+    public void setPlaceName(String placeName) {
+        this.placeName = placeName;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+}
---
+++ app/src/main/java/com/cookandroid/travelerapplication/task/Recommend_Place.java
@@ -0,0 +1,156 @@
+package com.cookandroid.travelerapplication.task;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.cookandroid.travelerapplication.mission.Mission;
+import com.cookandroid.travelerapplication.recommend.PlaceScore;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+// SlectData_...과 동일
+public class Recommend_Place extends AsyncTask<String,Void,String> { // 통신을 위한 InsertData 생성
+    ProgressDialog progressDialog;
+    private static String TAG = "youn"; //phptest log 찍으려는 용도
+
+    public ArrayList articleArrayList;
+
+    public <T> Recommend_Place(ArrayList<T> articleArrayList) {
+        this.articleArrayList = articleArrayList;
+    }
+
+    private String return_string = "";
+    @Override
+    protected String doInBackground(String... params) {
+        String serverURL = (String) params[0];
+
+        String postParameters = "";
+        try {
+            String user_id = (String) params[1];
+            postParameters ="user_id="+user_id;
+        }catch (Exception e){
+        }
+
+        try{ // HttpURLConnection 클래스를 사용하여 POST 방식으로 데이터를 전송한다.
+            URL url = new URL(serverURL); //주소가 저장된 변수를 이곳에 입력한다.
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+            httpURLConnection.setReadTimeout(5000); //5초안에 응답이 오지 않으면 예외가 발생한다.
+
+            httpURLConnection.setConnectTimeout(5000); //5초안에 연결이 안되면 예외가 발생한다.
+
+            httpURLConnection.setRequestMethod("POST"); //요청 방식을 POST로 한다.
+
+            httpURLConnection.connect();
+
+            OutputStream outputStream = httpURLConnection.getOutputStream();
+
+            //전송할 데이터가 저장된 변수를 이곳에 입력한다. 인코딩을 고려해줘야 하기 때문에 UTF-8 형식으로 넣어준다.
+            outputStream.write(postParameters.getBytes("UTF-8"));
+
+            outputStream.flush();//현재 버퍼에 저장되어 있는 내용을 클라이언트로 전송하고 버퍼를 비운다.
+            outputStream.close(); //객체를 닫음으로써 자원을 반납한다.
+
+
+            int responseStatusCode = httpURLConnection.getResponseCode(); //응답을 읽는다.
+            Log.d(TAG, "POST response code-" + responseStatusCode);
+
+            InputStream inputStream;
+
+            if(responseStatusCode == httpURLConnection.HTTP_OK){ //만약 정상적인 응답 데이터 라면
+                inputStream=httpURLConnection.getInputStream();
+                Log.d("flask정상: ","정상적으로 출력"); //로그 메세지로 정상적으로 출력을 찍는다.
+            }
+            else {
+                inputStream = httpURLConnection.getErrorStream(); //만약 에러가 발생한다면
+                Log.d("flask비정상: ","비정상적으로 출력"); // 로그 메세지로 비정상적으로 출력을 찍는다.
+            }
+
+            // StringBuilder를 사용하여 수신되는 데이터를 저장한다.
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+
+            while ((line = bufferedReader.readLine()) !=null ) {
+                sb.append(line);
+            }
+
+            bufferedReader.close();
+
+            Log.d("flask 값 :", sb.toString());
+
+            try{
+                parseJSONArray(sb.toString());
+            }catch (Exception e){
+                Log.d("youn", "JSON Error\n");
+
+            }
+
+
+
+
+            //저장된 데이터를 스트링으로 변환하여 리턴값으로 받는다.
+            return  sb.toString();
+
+
+        }
+
+        catch (Exception e) {
+
+            Log.d(TAG, "Recommend_Place: Error",e);
+
+            return  new String("Error " + e.getMessage());
+
+        }
+
+    }
+
+    private void parseJSONArray(String result) throws JSONException {
+        Log.d("younn", "찐처음");
+        // JSON 형태의 데이터를 파싱하여 JSONArray로 변환
+        JSONArray jsonArray = new JSONArray(result);
+        Log.d("younn", "찐처음: "+jsonArray.length()+"개");
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+            Log.d("younn", "처음");
+            PlaceScore placeScore = new PlaceScore();
+
+            Log.d("younn", "중간1");
+            String placeName = jsonObject.getString("placeName");
+            Log.d("younn", "중간2");
+            String score = jsonObject.getString("score");
+            Log.d("younn", "중간3");
+            placeScore.setPlaceName(placeName);
+            Log.d("younn", "중간4");
+            placeScore.setScore(score);
+            Log.d("younn", "중간5");
+
+            articleArrayList.add(placeScore);
+            Log.d("younn", "통과");
+
+            Log.d("younn", articleArrayList.toString());
+
+        }
+
+    }
+
+    public String get_return_string(){
+        return return_string;
+    }
+
+}
