6e6fa12978a1092cb7703b5b8ba9976cd966eeb4
서정후 <jeonghu1010@naver.com>
2023-09-16 16:47:36 +0900

    planning_main page UI update

---
+++ app/src/main/java/com/cookandroid/travelerapplication/planning/PlanningMainActivity.java
@@ -0,0 +1,15 @@
+package com.cookandroid.travelerapplication.planning;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.cookandroid.travelerapplication.R;
+
+public class PlanningMainActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_planning_main);
+
+    }
+}
